01f47cdd2424ebe1e9f327d2f9fa2e49480d4d1a
Vishal Jaiswada <vp022570@gmail.com>
2024-10-16 19:23:40 +0530

    added lines in index.js

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+//create middleware by developer
+// const vishalMiddleware = (req,res,next)=>{
+//     console.log(req)
+//     next()
+// }
+
+// we will use built-in middleware
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/hello/:name', (req, res) => {
+  res.send('Hello Friend!'+req.params.name)
+})
+
+app.get('/About', (req, res) => {
+     //res.send("About")
+     // res.sendFile(path.join(__dirname,'index.html'))
+     //res.status(403)
+     res.json({"Vishal":27})
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
